bbd94b6aba1f3521f8eae745e0baead06619c672
井口みずき <g3122021a4@edu.teu.ac.jp>
2022-10-11 01:19:09 +0900

    【リファクタ】巻数を指定するPickerを別のファイルにした。

--- MySerieses.swiftpm/DetailSettingView.swift
+++ MySerieses.swiftpm/DetailSettingView.swift
@@ -11,18 +11,6 @@ struct DetailSettingView: View {
     var lastNum: Int
     @State var num: Int = 0
     
-    func blackOrGray(fibI: Int) -> Bool
-    {
-        for nnl in noNumList
-        {
-            if fibI == nnl || lastNum < fibI
-            {
-                return true
-            }
-        }
-        return false
-    }
-    
     var body: some View {
         VStack
         {
@@ -55,24 +43,7 @@ struct DetailSettingView: View {
             }
             if $seriesOrNot.wrappedValue == true
             {
-                Picker("番号", selection: $num)
-                {
-                    let numMinusTwo: Int = num-2 > 0 ? num-2 : 1
-                    ForEach(numMinusTwo ... num+2, id:\.self)
-                    { i in
-                        Text(String(i))
-                        .foregroundColor(blackOrGray(fibI: i) ? .black : Color(red: 0.8, green: 0.8, blue: 0.8))
-                    }
-                }
-                .pickerStyle(.wheel)
-                //上の方押すと勝手に回っちゃう
-                .frame(height: 60)
-                //機能しないらしい.compositingGroup()
-                .clipped()
-                .onAppear()
-                {
-                    num = lastNum + 1
-                }
+                SeriesNumPicker(noNumList: noNumList, lastNum: lastNum, num: $num)
             }
             else
             {
