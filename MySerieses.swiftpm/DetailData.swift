154a9619c4d5db13ffb38142937a716fc32aa3b8
井口みずき <g3122021a4@edu.teu.ac.jp>
2023-01-27 11:00:23 +0900

    【コミット忘れ】フォルダー移動したからそりゃ消えてる

--- MySerieses.swiftpm/DetailData.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct DetailData : Identifiable, Codable, Equatable
-{
-    var id: String = UUID().uuidString
-    var title: String
-    var message: String
-}
--- MySerieses.swiftpm/SeriesesAndDetailsData.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-struct SeriesesAndDetailsData : Identifiable, Codable, Equatable
-{
-    var id: String = UUID().uuidString
-    var serieses: [SeriesData]
-    var details: [DetailData]
-}
